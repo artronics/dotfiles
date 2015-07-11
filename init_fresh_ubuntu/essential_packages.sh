afb7e73bda685e54d851de276d976cb7341cee69
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-07-11 11:06:48 -0700

    Init commit. Basic vim configuration. add some scripts to bin folder

---
+++ init_fresh_ubuntu/essential_packages.sh
@@ -0,0 +1,7 @@
+apt-get update
+
+apt-get install -y zsh git vim default-jre default-jdk nodejs npm terminator 
+
+#Getting and installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
