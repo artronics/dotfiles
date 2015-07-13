70b2a6b1f74c07633f0d07cfb888278ecde1997f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-07-12 18:28:43 -0700

    change ohmyzsh theme to avit

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,10 +11,12 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gcln='git clone'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gcma='git commit -am'
 alias gca='git commit -a'
+
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
