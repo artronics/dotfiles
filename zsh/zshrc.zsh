5bd42f6fb823d68af010e952d565da712b9db97d
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-07-20 02:20:29 +0200

    change source: oh-my-zsh to zshrc.sylimk so we can override it easily

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -53,9 +53,8 @@ plugins=(git)
 
 # User configuration
 
+#
 # export MANPATH="/usr/local/man:$MANPATH"
-
-source $ZSH/oh-my-zsh.sh
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
